68e8d06589bc763116eb390994d8d739b5098b36
reito <cnschwarzer@qq.com>
2024-04-27 13:30:24 +0800

    Init

--- vite.config.js
+++ vite.config.js
@@ -13,5 +13,6 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  publicDir: process.env.NODE_ENV === "production" ? "/feishu-doc-uuid/" : "/"
 })
